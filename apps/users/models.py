3b7f9a70008d3d773203b4bf722c0a7a04a3b4e7
unknown <danny213697@gmail.com>
2022-07-31 15:22:20 +0600

    dsfdsfsdfsdfsdsdfsdfsdfasdasd

--- apps/users/models.py
+++ apps/users/models.py
@@ -16,7 +16,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    date_of_birth = models.DateTimeField(verbose_name='Дата рождения', null=True)
+    date_of_birth = models.DateField(verbose_name='Дата рождения', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     phone_number = models.CharField(max_length=255,  unique=True, null=True)
