d409d485c63f6ebb44c20e564c1eb112a0a76c87
unknown <danny213697@gmail.com>
2022-08-10 19:03:50 +0600

    fixed heroku problems

--- diploma/wsgi.py
+++ diploma/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'diploma.settings')
 
 application = get_wsgi_application()
+
+
+SECRET_KEY = os.environ['SECRET_KEY']
